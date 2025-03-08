b3a7f4faac2631ec66b6a116b914bfffa08d9a1b
Huzzanss <huzzans.hafidz@gmail.com>
2025-03-08 13:29:26 +0800

    GameListRonix.lua

---
+++ GameListRonix.lua
@@ -0,0 +1,12 @@
+local Games = {
+  [2753915549] = "https://api.luarmor.net/files/v3/loaders/fe9186f93ffe4000120ba5db7ef9443b.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://api.luarmor.net/files/v3/loaders/fe9186f93ffe4000120ba5db7ef9443b.lua", -- Blox Fruit Sea 2
+  [7449423635] = "https://api.luarmor.net/files/v3/loaders/fe9186f93ffe4000120ba5db7ef9443b.lua", -- Blox Fruit Sea 3
+  [17625359962] = "https://api.luarmor.net/files/v3/loaders/e945f55997c4240abc865c0bcc2136c5.lua",  -- Rivals
+  [13772394625] = "https://api.luarmor.net/files/v3/loaders/79ab2d3174641622d317f9e234797acb.lua",  -- Blade Ball
+  [286090429] = "https://api.luarmor.net/files/v3/loaders/93f86be991de0ff7d79e6328e4ceea40.lua",  -- Arsenal
+  [16732694052] = "https://api.luarmor.net/files/v3/loaders/280ebe55d6aa339b1330231ad0eae889.lua", -- Fisch
+  [116495829188952] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+  [70876832253163] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+  [1537690962] = "https://api.luarmor.net/files/v3/loaders/9cfaf7193fd721277d8a7b8bbf394315.lua" -- Bee Swarm Simulator
+}
